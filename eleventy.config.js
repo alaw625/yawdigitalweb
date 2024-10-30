4d9c9b0392a8233548d072f14b4d34790de0982f
alex <alaw625@gmail.com>
2024-10-31 03:34:21 +0800

    initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+module.exports = function (eleventyConfig) {
+  //enable shortcode to simplfy adding svg to the templates e.g. {% svg "myfile" %}
+  let getSvgContent = function (file) {
+    let relativeFilePath = `./src/svg/${file}.svg`;
+    let data = fs.readFileSync(relativeFilePath, function (err, contents) {
+      if (err) return err;
+      return contents;
+    });
+    return data.toString("utf8");
+  };
+  eleventyConfig.addShortcode("svg", getSvgContent);
+
+  /* Pass Through File Copy */
+  eleventyConfig.addPassthroughCopy("src/_includes/assets/images");  
+  eleventyConfig.addPassthroughCopy("src/_includes/assets/js");
+  eleventyConfig.addPassthroughCopy("src/style.css");
+
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data",
+      output: "_site",
+    },
+  };
+};
